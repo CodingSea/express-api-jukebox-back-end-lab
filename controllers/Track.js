8c4268fe5ec380ec163005e37adc3561f4eb545c
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-08-12 21:33:02 +0300

    created the router routes

--- controllers/Track.js
+++ controllers/Track.js
@@ -1,2 +1,70 @@
 const Track = require("../models/Track");
 
+async function createTrack(req, res)
+{
+    try
+    {
+
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+async function indexTrack(req, res)
+{
+    try
+    {
+
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+async function showTrack(req, res)
+{
+    try
+    {
+
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+async function updateTrack(req, res)
+{
+    try
+    {
+
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+async function deleteTrack(req, res)
+{
+    try
+    {
+
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+module.exports = 
+{
+    createTrack,
+    indexTrack,
+    showTrack,
+    updateTrack,
+    deleteTrack
+}
--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const trackController = require("../controllers/Track");
 
+router.post("/new", trackController.createTrack);
 
+router.post("/", trackController.indexTrack);
 
+router.post("/:id", trackController.showTrack);
+
+router.put("/:id", trackController.updateTrack);
+
+router.delete("/:id", trackController.deleteTrack);
 
 
 module.exports = router;
