3ee3df256c2a7ef7718b1eec3cec110e8dcc063a
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-08-12 22:02:51 +0300

    coded updateTrack and deleteTrack in track controller

--- controllers/Track.js
+++ controllers/Track.js
@@ -58,11 +58,19 @@ async function updateTrack(req, res)
 {
     try
     {
-
+        const track = await Track.findByIdAndUpdate(req.params.id, req.body, {new:true});
+        if(track)
+        {
+            res.status(200).json(track);
+        }
+        else
+        {
+            res.status(404);
+        }
     }
     catch(error)
     {
-        console.log(error);
+        res.status(500).json({ error: error.message });
     }
 }
 
@@ -70,11 +78,19 @@ async function deleteTrack(req, res)
 {
     try
     {
-
+        const track = await Track.findByIdAndDelete(req.params.id);
+        if(track)
+        {
+            res.status(200).json(track);
+        }
+        else
+        {
+            res.status(404);
+        }
     }
     catch(error)
     {
-        console.log(error);
+        res.status(500).json({ error: error.message });
     }
 }
 
