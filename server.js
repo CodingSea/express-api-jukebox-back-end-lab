62d1730b9d35239f7a8b1b9de0185076661e59a8
Coding Sea <46266365+CodingSea@users.noreply.github.com>
2025-08-12 20:40:57 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+mongoose.connect(process.env.DB);
+mongoose.connection.on("connected", () =>
+{
+    console.log("Connected to DB");
+})
+
+
+app.listen(process.env.PORT, () =>
+{
+    console.log("Listening to Port");
+})
